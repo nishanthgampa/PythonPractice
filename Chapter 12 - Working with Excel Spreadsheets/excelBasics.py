66224ef19aecb6d5ff0c6ea459a0dd977d2b286d
Nishanth Gampa <nishanthgampa@gmail.com>
2019-09-05 19:03:28 -0700

    more practive on working with excel spreadsheets

--- Chapter 12 - Working with Excel Spreadsheets/excelBasics.py
+++ Chapter 12 - Working with Excel Spreadsheets/excelBasics.py
@@ -40,3 +40,51 @@ c.value
 'Row ' + str(c.row) + ', Column ' + str(c.column) + ' is ' + str(c.value)
 
 'Cell ' + c.coordinate + ' is ' + c.value
+
+sheet['C1'].value
+
+sheet.cell(row = 1, column = 2).value
+
+for i in range(1,8,2):
+    print(i,sheet.cell(row = i, column=2).value)
+    
+sheet.max_row
+
+sheet.max_column
+
+from openpyxl.utils import get_column_letter, column_index_from_string
+
+get_column_letter(900)
+
+column_index_from_string('AA')
+
+tuple(sheet['A1':'C3'])
+
+for rowOfCellObjects in sheet['A1':'C3']:
+    for cellObj in rowOfCellObjects:
+        print(cellObj.coordinate, cellObj.value)
+    print("#######End of Row########")
+
+
+for i in sheet['B']:
+    print(i.value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
